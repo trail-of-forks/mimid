4932f9953e041e2404a58ac50f2eb9c6493e5b35
Evan Sultanik <evan.sultanik@trailofbits.com>
2020-07-08 14:39:06 -0400

    Added a Python setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mimid',
+    description='A library for mining grammars from program traces.',
+    url='https://github.com/trailofbits/mimid',
+    author='Rahul Gopinath and Trail of Bits',
+    version='0.1',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'pudb'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'The Fuzzing Book License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities'
+    ]
+)
